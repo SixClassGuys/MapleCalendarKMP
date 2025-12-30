301df3860de0e36caa04d68698fc24dc285930fc
littlesam95 <littlesam95@naver.com>
2025-12-30 16:03:57 +0900

    [feat] Composable에서 FCM 전송 성공 여부 확인
    - 또한 MainViewModel 선언(추후 구현 예정)

--- composeApp/src/androidMain/kotlin/com/sixclassguys/maplecalendar/App.kt
+++ composeApp/src/androidMain/kotlin/com/sixclassguys/maplecalendar/App.kt
@@ -13,15 +13,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.sixclassguys.maplecalendar.domain.model.ApiState
+import com.sixclassguys.maplecalendar.presentation.NotificationViewModel
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import maplecalendar.composeapp.generated.resources.Res
 import maplecalendar.composeapp.generated.resources.compose_multiplatform
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
 
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 @Preview
-fun App() {
+fun App(
+    notificationViewModel: NotificationViewModel = koinViewModel()
+) {
+    val uiState by notificationViewModel.registrationState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        notificationViewModel.initNotification()
+    }
+
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
@@ -33,6 +47,12 @@ fun App() {
         ) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
+            }
+            when (uiState) {
+                is ApiState.Loading -> { Text("로딩") }
+                is ApiState.Success -> { Text("성공") }
+                is ApiState.Error -> { Text("에러") }
+                is ApiState.Idle -> { Text("초기 상태") }
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
---
+++ shared/src/commonMain/kotlin/com/sixclassguys/maplecalendar/presentation/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.sixclassguys.maplecalendar.presentation
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    // TODO: 앱의 전반적인 상태 관리
+}
