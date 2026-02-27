a87b2cbfc086787d789ef05470ccc35baf9a3e09
littlesam95 <littlesam95@naver.com>
2026-02-27 16:59:50 +0900

    [chore] 버전 변경

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -103,8 +103,8 @@ android {
         applicationId = "com.sixclassguys.maplecalendar"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 10
-        versionName = "0.3.0"
+        versionCode = 11
+        versionName = "0.3.1"
 
         ndk {
             // 구글 플레이에서 요구하는 64비트 아키텍처를 포함합니다.
